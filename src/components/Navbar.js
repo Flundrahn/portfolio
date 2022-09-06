26c4e218f53c4903687bb33a6bc18c01f65175e2
Flundrahn <flundst@gmail.com>
2022-09-06 14:54:03 +0200

    cleanup & add navbar with links

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Navbar.css';
+
+function Icon({ href, icon }) {
+  const size = '1x';
+
+  return (
+    <div className="navbar__icon">
+      <a
+        href={href}
+        className="navbar__link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={icon} size={size} />
+      </a>
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    // NOTE Can add styling shake, bounce etc to draw attention to icons
+    <nav className="navbar">
+      <Icon href="https://github.com/Flundrahn" icon={faGithub} />
+      <Icon href="https://www.linkedin.com/in/fredrik-lundstrom/" icon={faLinkedin} />
+      <Icon href="mailto:fredrik.lundstrom@appliedtechnology.se" icon={faEnvelope} />
+    </nav>
+  );
+}
+
+export default Navbar;
--- todo.md
+++ todo.md
@@ -1,14 +1,15 @@
 # ToDo
 
 ## MVP
-* Navbar with icons to linkedin, github & e-mail (fontawesome icons)
-* Textbox with name & introduction
+* [x] Navbar with icons to linkedin, github & e-mail (fontawesome icons)
 * Profile photo
+* Textbox with name & introduction
 * Timeline header "My projects"
 * Timeline objects
     - Header
     - Description
     - Link to GitHub, Live Demo, Video, 
+* Choose font
 
 Want-to-do
 * Animated transition when click on timeline objects ("section full-screen")
